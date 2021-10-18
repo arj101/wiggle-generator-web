c834d983ba31d6442d2ec75e337e0deb68884f31
arduino_explorer <42172864+arj101@users.noreply.github.com>
2021-10-18 18:29:00 +0530

    change wiggle progressive append line count

--- src/wiggle.js
+++ src/wiggle.js
@@ -12,7 +12,7 @@ async function generateWiggle(text, rate, lineCount, lineLength) {
 
         let appendProgressive = () => {
             let iStart = i;
-            let maxCount = iStart + 750;
+            let maxCount = iStart + 500;
             while (i < maxCount && i < lineCount) {
                 output += ' '.repeat((Math.sin(x) * radius ) + radius) + text + '\n';
                 x += rate;
